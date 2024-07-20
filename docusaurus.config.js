1e9cc7ee72f77740fe8567b7532645ae6d883610
lalifeier <lalifeier@gmail.com>
2024-07-20 16:36:31 +0800

    feat: init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Blog',
   tagline: 'Blog',
   url:  process.env.BASE === 'zone' ? 'https://docs.lalifeier.io' : 'https://lalifeier.github.io',
-  baseUrl: '/blog/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
